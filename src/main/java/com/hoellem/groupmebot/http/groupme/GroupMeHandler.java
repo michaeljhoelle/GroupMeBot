0378ca50fe734bc84d2f6c0a28b50cef70a51339
Mike <michaeljhoelle@gmail.com>
2020-09-01 17:24:27 -0400

    Fixed response

--- src/main/java/com/hoellem/groupmebot/http/groupme/GroupMeHandler.java
+++ src/main/java/com/hoellem/groupmebot/http/groupme/GroupMeHandler.java
@@ -5,10 +5,11 @@ import com.hoellem.groupmebot.http.BaseHandler;
 public class GroupMeHandler extends BaseHandler
 {
   public void handle(GroupMeRequest request) {
-    String response = restTemplate.postForObject(url, request.getAvatar(), String.class);
-    if (response != null)
+    GroupMeResponse groupMeResponse = new GroupMeResponse(botId, request.getAvatar());
+    String finalResponse = restTemplate.postForObject(url, groupMeResponse, String.class);
+    if (finalResponse != null)
     {
-      logger.info(response);
+      logger.info(finalResponse);
     }
   }
 }
--- src/main/java/com/hoellem/groupmebot/http/groupme/GroupMeResponse.java
+++ src/main/java/com/hoellem/groupmebot/http/groupme/GroupMeResponse.java
@@ -1,5 +1,28 @@
 package com.hoellem.groupmebot.http.groupme;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class GroupMeResponse
 {
+  private final String botId;
+  private final String text;
+
+  public GroupMeResponse(String botId, String text)
+  {
+    this.botId = botId;
+    this.text = text;
+  }
+
+  @JsonProperty("bot_id")
+  public String getBotId()
+  {
+    return botId;
+  }
+
+  @JsonProperty("text")
+  public String getText()
+  {
+    return text;
+  }
+
 }

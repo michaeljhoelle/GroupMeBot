c5c3b6cdfc7152d15080fefb116d047d8b6377e1
Mike <michaeljhoelle@gmail.com>
2022-12-02 21:07:10 -0500

    whoops

--- src/main/java/com/hoellem/groupmebot/model/groupme/Command.java
+++ src/main/java/com/hoellem/groupmebot/model/groupme/Command.java
@@ -1,9 +1,12 @@
 package com.hoellem.groupmebot.model.groupme;
 
+import com.hoellem.groupmebot.model.reddit.Subreddit;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @RequiredArgsConstructor
@@ -12,7 +15,7 @@ public enum Command
     FEET(List.of("^/feet\\b", "^/feetcount\\b")),
     CLS(List.of("^/cls3\\b")),
     TODAY(List.of("^Is (?:it|today) ([^?]+)\\??")),
-    SUBREDDIT_POST(List.of("^/suffer\\b", "^/beans\\b")),
+    SUBREDDIT_POST(Arrays.stream(Subreddit.values()).map(Subreddit::getShortName).map(str -> "^/" + str + "\\b").collect(Collectors.toList())),
     SUBREDDIT_FIND(List.of("^r/\\w+$")),
     TODO(List.of("^/todo\\b"));
 
